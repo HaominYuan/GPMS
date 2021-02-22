f84bf44b957f5960a3bb0cd4c6fbd07b8c13197e
tstxxy <tstxxy@gmail.com>
2021-02-22 17:06:10 +0800

    commit

--- src/pages/seller/Seller.js
+++ src/pages/seller/Seller.js
@@ -1,8 +1,60 @@
+import { Menu } from 'antd'
+import { Link } from 'react-router-dom'
+import { Layout } from 'antd'
 
+const { Content, Sider } = Layout
+
+const { SubMenu } = Menu;
 
 const Seller = () => {
+
+    const handleClick = e => {
+        console.log('click ', e);
+    }
+
     return (
-        <h1>Seller</h1>
+        <>
+            <Layout>
+                <Sider className="site-layout-background">
+                    <Menu
+                        onClick={handleClick}
+                        defaultSelectedKeys={['type']}
+                        mode="inline"
+                        style={{ height: '100%' }}
+                    >
+                        <SubMenu key="flower" title="鲜花管理">
+                            <Menu.Item key="type">
+                                鲜花类别
+                            <Link to="/seller/type" />
+                            </Menu.Item>
+                            <Menu.Item key="flowerlist">
+                                鲜花列表
+                            <Link to="/seller/name" />
+                            </Menu.Item>
+                        </SubMenu>
+
+                        <SubMenu key="order" title="订单管理">
+                            <Menu.Item key="orderlist">
+                                订单列表
+                        <Link to="/seller/name" />
+                            </Menu.Item>
+                        </SubMenu>
+                    </Menu>
+                </Sider>
+                <Layout style={{ padding: '0 24px 24px' }}>
+                    <Content
+                        className="site-layout-background"
+                        style={{
+                            padding: 24,
+                            margin: 0,
+                            'min-height': '100vh'
+                        }}
+                    >
+                        Content
+                    </Content>
+                </Layout>
+            </Layout>
+        </>
     )
 }
 
